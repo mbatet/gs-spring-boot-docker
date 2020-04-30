6bd20afad779abada8637a16971b551fc09328e4
mbatet <mbatet@gmail.com>
2020-04-30 12:22:56 +0200

    First version. gc-spring-boot-docker with java 13 and Alpine distr.

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+
+		//Un cosa que es pot fer només desde Java 11,
+		// aixi ens assegurem que el host realment s'esta executant en un java superior a 8
+		var greeting = "Hello Docker World.  System.getProperty(\"java.version\") is....: " + System.getProperty("java.version");
+		return greeting;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
