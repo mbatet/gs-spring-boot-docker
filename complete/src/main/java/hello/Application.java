0f8af14d257b49b4d30f29805954e45f9399e180
mbatet <mbatet@gmail.com>
2020-05-01 22:20:45 +0200

    Canviem application.yaml per application.properties

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	@RequestMapping("/")
 	public String home() {
