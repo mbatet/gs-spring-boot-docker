bb22a3eb655d5dfea0ec8c46ec63f96c5cba77a4
mbatet <mbatet@gmail.com>
2020-05-04 16:13:37 +0200

    Creem un MainController

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,26 +1,11 @@
 package hello;
 
-import hello.model.Customer;
-import hello.repository.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
-@Controller
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
@@ -54,16 +39,7 @@ public class Application {
 	* */
 
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 
-		var message = "Hello Docker World.  System.getProperty(\"java.version\") is....: " + System.getProperty("java.version");
-
-		model.addAttribute("name", name);
-		model.addAttribute("message", message);
-
-		return "greeting";
-	}
 
 
 }
