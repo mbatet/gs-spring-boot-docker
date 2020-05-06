e67bda231495fff5f01c1aec95c405e6d28bbc8d
mbatet <mbatet@gmail.com>
2020-05-06 21:18:54 +0200

    spring security: WebSecurityConfig

--- complete/src/main/java/hello/controller/MainController.java
+++ complete/src/main/java/hello/controller/MainController.java
@@ -34,6 +34,17 @@ public class MainController {
     }
 
 
+    // Login form
+    @RequestMapping("/login.html")
+    public String login() {
+        return "login";
+    }
 
+    // Login form with error
+    @RequestMapping("/login-error.html")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
 
 }
